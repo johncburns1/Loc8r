897cc01581d3e5c1c20cc4ed069cdec98562add1
Jack Burns <Jack Burns>
2016-02-04 20:56:18 -0800

    new commit

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 var ctrlMain = require('../controllers/main');
 
+var homepageController = function(req, res) {
+  res.render('index', { title: 'Express'});
+};
+
 /* GET home page. */
 router.get('/', ctrlMain.index);
 
